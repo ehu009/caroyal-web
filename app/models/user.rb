6f5baaa9a001489858ae46b475b5e2aa11b4c8b7
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-01 16:56:57 +0100

    fixed some dumb typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, uniqueness: true
     validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
-    validates :password_digest, presence: :true
+    validates :password_digest, presence: true
 end
