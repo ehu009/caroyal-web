ab5b98f1bb3f53e3eb6bc59fca3f7aebef0e7582
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-01 14:33:43 +0100

    dummy controller for prices

---
+++ app/controllers/prices.rb
@@ -0,0 +1,35 @@
+class PricesController < ApplicationController
+
+    def index
+
+    end
+
+    def new
+        
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+
+    private
+    
+    def price_params
+
+
+    end
+
+end
