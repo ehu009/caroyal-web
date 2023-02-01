70da1c9eec4bd5b43ea5121df4f88245dc7c0d80
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-01 14:38:28 +0100

    model User validates email uniqueness + email format + password presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :email, uniqueness: true
+    validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+    validates :password_digest, presence: :true
 end
