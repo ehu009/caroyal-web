c8e12088a1392ba3712128a642f03af1d083ded7
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-01 10:02:11 +0100

    routes for users and their sessions

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,12 @@ Rails.application.routes.draw do
   get '/dev', to: 'application#dev'
   
 
+  resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+
+  get '/login', to: 'sessions#login'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
+
+  
 end
