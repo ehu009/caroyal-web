f96318b12a4f3eab0c69709dc1303525983ed2e9
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-08 10:19:19 +0100

    a test mail

--- app/mailers/no_reply_mailer.rb
+++ app/mailers/no_reply_mailer.rb
@@ -1,2 +1,14 @@
 class NoReplyMailer < ApplicationMailer
+    default from: email_address_with_name('no-reply@caroyal.no', "no-reply")
+
+    def email_confirm
+      @user = params[:user]
+      puts @user.email
+      #@url  = 'http://example.com/login'
+      mail(to: @user.email, subject: 'Welcome to My Awesome Site') #do |m|
+      #  m.html { render 'email_confirm' }
+      #  m.text { render plain: 'email_confirm yo'}
+      #end
+    end
+
 end
