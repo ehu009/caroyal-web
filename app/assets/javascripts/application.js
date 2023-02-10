dac1e1d2d6f4ccf3f0d103859dd72e982d04b09e
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-10 19:03:52 +0100

    moved javascript from layouts to assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,21 @@
 //= require chartkick
 //= require Chart.bundle
+
+  function open_nav(id) {
+    var e = document.getElementById(id);
+    e.classList.add('nav-veil-fadein');
+    e.classList.remove('nav-veil-fadeout');
+  }
+  function close_nav(id) {
+    var e = document.getElementById(id);
+    e.classList.remove('nav-veil-fadein');
+    e.classList.add('nav-veil-fadeout');
+  }
+  function unveil(id) {
+    var e = document.getElementById(id);
+    e.classList.add('veil-fadeout');
+  }
+  function remove_toast(id) {
+    var e = document.getElementById(id);
+    e.classList.remove('show');
+  }
