631a61d067ce8dea09f324a03fe3c1678cc51000
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-10 12:51:50 +0100

    added extra fields to user model

---
+++ db/migrate/20230210113040_add_contact_details_to_user.rb
@@ -0,0 +1,11 @@
+class AddContactDetailsToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name_prefix, :string
+    add_column :users, :full_name, :string
+    add_column :users, :age, :number
+    add_column :users, :phone_number, :number
+    add_column :users, :address, :string
+    add_column :users, :country, :string
+    add_column :users, :city, :string
+  end
+end
