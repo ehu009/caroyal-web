5dc7c47218b849999c1108681e860ff076d1d060
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-12 17:42:18 +0100

    typo

--- db/migrate/20230212101925_add_questions_to_questionaires.rb
+++ db/migrate/20230212101925_add_questions_to_questionaires.rb
@@ -1,18 +1,18 @@
 class AddQuestionsToQuestionaires < ActiveRecord::Migration[7.0]
   def change
-    add_column :distributor_questionaire, :length, :number
-    add_column :distributor_questionaire, :yearly_amount, :number
-    add_column :distributor_questionaire, :other_products, :string
-    add_column :distributor_questionaire, :pays_deposit, :boolean
-    add_column :distributor_questionaire, :pays_wire, :boolean
-    add_column :distributor_questionaire, :pays_card, :boolean
-    add_column :distributor_questionaire, :pays_crypto, :boolean
+    add_column :distributor_questionaires, :length, :number
+    add_column :distributor_questionaires, :yearly_amount, :number
+    add_column :distributor_questionaires, :other_products, :string
+    add_column :distributor_questionaires, :pays_deposit, :boolean
+    add_column :distributor_questionaires, :pays_wire, :boolean
+    add_column :distributor_questionaires, :pays_card, :boolean
+    add_column :distributor_questionaires, :pays_crypto, :boolean
     
-    add_column :producer_questionaire, :commodities, :string
-    add_column :producer_questionaire, :volume, :number
-    add_column :producer_questionaire, :pays_deposit, :boolean
-    add_column :producer_questionaire, :pays_wire, :boolean 
-    add_column :producer_questionaire, :pays_card, :boolean
-    add_column :producer_questionaire, :pays_crypto, :boolean
+    add_column :producer_questionaires, :commodities, :string
+    add_column :producer_questionaires, :volume, :number
+    add_column :producer_questionaires, :pays_deposit, :boolean
+    add_column :producer_questionaires, :pays_wire, :boolean 
+    add_column :producer_questionaires, :pays_card, :boolean
+    add_column :producer_questionaires, :pays_crypto, :boolean
   end
 end
