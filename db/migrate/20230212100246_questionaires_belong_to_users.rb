27349e0d72dc6a7793069279262d59e569c6c539
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-12 19:37:50 +0100

    proper references for questionaires

--- db/migrate/20230212100246_questionaires_belong_to_users.rb
+++ db/migrate/20230212100246_questionaires_belong_to_users.rb
@@ -1,9 +1,6 @@
 class QuestionairesBelongToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_reference :producer_questionaires, :user
-    add_foreign_key :producer_questionaires, :user
-
-    add_reference :distributor_questionaires, :user
-    add_foreign_key :distributor_questionaires, :user
+    add_reference :producer_questionaires, :user, null: false, foreign_key: true
+    add_reference :distributor_questionaires, :user, null: false, foreign_key: true
   end
 end
