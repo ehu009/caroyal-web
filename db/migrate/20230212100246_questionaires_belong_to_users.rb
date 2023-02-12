d1d6945dac2a873e3d908aeb3e0a4a84a83d53f6
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-12 12:13:43 +0100

    sick typos

--- db/migrate/20230212100246_questionaires_belong_to_users.rb
+++ db/migrate/20230212100246_questionaires_belong_to_users.rb
@@ -1,9 +1,9 @@
 class QuestionairesBelongToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_reference :producer_questionares, :user
-    add_foreign_key :producer_questionares, :user
+    add_reference :producer_questionaires, :user
+    add_foreign_key :producer_questionaires, :user
 
-    add_reference :distributor_questionares, :user
-    add_foreign_key :distributor_questionares, :user
+    add_reference :distributor_questionaires, :user
+    add_foreign_key :distributor_questionaires, :user
   end
 end
--- db/migrate/20230212101925_add_questions_to_questionaires.rb
+++ db/migrate/20230212101925_add_questions_to_questionaires.rb
@@ -1,18 +1,18 @@
 class AddQuestionsToQuestionaires < ActiveRecord::Migration[7.0]
   def change
-    add_column :distributor_questionare, :length, :number
-    add_column :distributor_questionare, :yearly_amount, :number
-    add_column :distributor_questionare, :other_products, :string
-    add_column :distributor_questionare, :pays_deposit, :boolean
-    add_column :distributor_questionare, :pays_wire, :boolean
-    add_column :distributor_questionare, :pays_card, :boolean
-    add_column :distributor_questionare, :pays_crypto, :boolean
+    add_column :distributor_questionaire, :length, :number
+    add_column :distributor_questionaire, :yearly_amount, :number
+    add_column :distributor_questionaire, :other_products, :string
+    add_column :distributor_questionaire, :pays_deposit, :boolean
+    add_column :distributor_questionaire, :pays_wire, :boolean
+    add_column :distributor_questionaire, :pays_card, :boolean
+    add_column :distributor_questionaire, :pays_crypto, :boolean
     
-    add_column :producer_questionare, :commodities, :string
-    add_column :producer_questionare, :volume, :number
-    add_column :producer_questionare, :pays_deposit, :boolean
-    add_column :producer_questionare, :pays_wire, :boolean 
-    add_column :producer_questionare, :pays_card, :boolean
-    add_column :producer_questionare, :pays_crypto, :boolean
+    add_column :producer_questionaire, :commodities, :string
+    add_column :producer_questionaire, :volume, :number
+    add_column :producer_questionaire, :pays_deposit, :boolean
+    add_column :producer_questionaire, :pays_wire, :boolean 
+    add_column :producer_questionaire, :pays_card, :boolean
+    add_column :producer_questionaire, :pays_crypto, :boolean
   end
 end
