73463661617e7cc280e5cc82ab0fdacbc84155eb
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-15 14:20:06 +0100

    do not validate email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :email
     validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
     validates :password_digest, presence: true
 
