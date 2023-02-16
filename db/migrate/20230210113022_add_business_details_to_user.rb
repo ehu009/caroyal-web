678bfcfff692c069e0c8221fee865fed8d22e5cb
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-16 12:02:30 +0100

    decimals replaced by integers

--- db/migrate/20230210113022_add_business_details_to_user.rb
+++ db/migrate/20230210113022_add_business_details_to_user.rb
@@ -1,6 +1,6 @@
 class AddBusinessDetailsToUser < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :company_address, :string
-    add_column :users, :tax_identification_number, :number
+    add_column :users, :tax_identification_number, :integer
   end
 end
--- db/migrate/20230210113040_add_contact_details_to_user.rb
+++ db/migrate/20230210113040_add_contact_details_to_user.rb
@@ -3,7 +3,7 @@ class AddContactDetailsToUser < ActiveRecord::Migration[7.0]
     add_column :users, :name_prefix, :string
     add_column :users, :full_name, :string
     add_column :users, :age, :number
-    add_column :users, :phone_number, :number
+    add_column :users, :phone_number, :integer
     add_column :users, :address, :string
     add_column :users, :country, :string
     add_column :users, :city, :string
