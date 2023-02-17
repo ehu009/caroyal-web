9bd0ec83e589d243496f26b2bef67619ef323766
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-17 10:04:34 +0100

    import error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -57,3 +57,54 @@ function getCityData(data_url) {
     console.log(dataset[4])
     return dataset;*/
 }
+
+
+function initCitySelect(elem) {
+  var opt = document.createElement("option");
+  opt.disabled = true;
+  opt.text = "Select a country first";
+  elem.appendChild(opt);
+}
+function resetCitySelect(elem) {
+  elem.innerHTML = '';
+  initCitySelect(elem);
+}
+function addCitySelectOption(elem, option) {
+  var opt = document.createElement("option");
+  opt.value = option;
+  opt.text = option;
+  elem.appendChild(opt);
+}
+
+function populateCitySelect(elem, countryCode) {
+  var filtered = cityData.filter(city => city['country'] == countryCode);
+  var l = [];
+  for (e of filtered) {
+      l.push(e['name']);
+  }
+  for (e of l.sort()) {
+      addCitySelectOption(elem, e);
+  }
+}
+
+function initCitySelector(countrySelectId, citySelectId, initCountry, initCity) {
+  var country_field = document.getElementById(countrySelectId);
+  var city_field = document.getElementById(citySelectId);
+
+  initCitySelect(city_field);
+  
+  addEventListener("city-data-available",
+      () => {      
+          if (initCountry != "") {
+              if (initCity != "" | initCity != undefined) {
+                  populateCitySelect(city_field, initCountry);
+                  city_field.value = initCity;
+              }
+          }
+          country_field.onchange = function () {
+              resetCitySelect(city_field);
+              populateCitySelect(city_field, country_field.value);
+          };
+      },
+  false);
+}
