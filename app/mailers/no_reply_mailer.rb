addcc2296c580921dc4804d4574fbfec25eebd26
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-19 23:27:36 +0100

    pass user to no-reply mailer as parameter

--- app/mailers/no_reply_mailer.rb
+++ app/mailers/no_reply_mailer.rb
@@ -1,22 +1,21 @@
 class NoReplyMailer < ApplicationMailer
 
-    def email_confirmation
-      @user = params[:user]
-      
+    def email_confirmation user
       o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
       @token = (0...25).map { o[rand(o.length)] }.join
 
       from = Email.new(email: 'no-reply@caroyal.com')
-      to = Email.new(email: @user.email)
-      content = Content.new(type: 'text/html', value: render_to_string(:email_confirmation))
+      to = Email.new(email: user.email)
+      content = Content.new(type: 'text/html', value: render_to_string(:email_c>
 
       mail = SendGrid::Mail.new(from, 'Confirm you email address', to, content)
       sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
       response = sg.client.mail._('send').post(request_body: mail.to_json)
 
-      @user.confirmation_sent_at = Time.now()
-      @user.confirmation_token = @token
-      @user.save
+      user.confirmation_sent_at = Time.now()
+      user.confirmation_token = @token
+      user.save
+
     end
 
 end
