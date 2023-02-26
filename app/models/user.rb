b213849bfd6bf5a4dff371177c0eaab05952605c
Eirik Husebø <jenssen.eirik@gmail.com>
2023-02-26 16:47:52 +0100

    clean up user model a little

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,21 @@
 class User < ApplicationRecord
     has_secure_password
-    validate :check_email
     validates :password_digest, presence: true
+
+    validates :phone_number, uniqueness: true, presence: true
+    validates :phone_code, presence: true
+    validate :check_email
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :name_prefix, presence: true
 
     validates :company_name, presence: true
     validates :company_address, presence: true
-    validates :tax_identification_number, presence: true
-    validates :name_prefix, presence: true
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :phone_number, uniqueness: true, presence: true
     validates :company_country, presence: true
     validates :company_city, presence: true
-    validates :phone_code, presence: true
+    validates :tax_identification_number, presence: true
+    
 
     has_one :distributor_questionare
     has_one :producer_questionare
