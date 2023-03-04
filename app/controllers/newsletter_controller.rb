0ceae5b61c8a5b9bf610d6ede91627c5b7d81300
Eirik Husebø <jenssen.eirik@gmail.com>
2023-03-04 10:28:59 +0100

    only admins may manipulate newsletters

--- app/controllers/newsletter_controller.rb
+++ app/controllers/newsletter_controller.rb
@@ -1,5 +1,7 @@
 class NewsletterController < ApplicationController
 
+    before_action :confirm_admin
+
     def index
         @newsletters = Newsletter.all
     end
@@ -28,6 +30,17 @@ class NewsletterController < ApplicationController
 
     def newsletter_params
 
+    end
+
+    def confirm_admin
+        id = session[:user_id]
+        
+        if id != nil then
+            user = User.find_by_id id
+            if user.administrator == false then
+                redirect_to root_path, notice: "Sorry - that page is for administrators."
+            end
+        end
     end
     
 end
