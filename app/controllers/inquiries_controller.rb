7d777e66e9e78764e4f53d1780ff773c37cf5642
Eirik Husebø <jenssen.eirik@gmail.com>
2023-03-10 13:07:45 +0100

    implemented controller for inquiries

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,22 +1,69 @@
 class InquiriesController < ApplicationController
   def index
+    @todo = Inquiry.where status: "todo"
+    @done = Inquiry.where status: "done"
   end
 
   def show
+    @inq = Inquiry.find params[:id]
   end
 
   def new
+    @inq = Inquiry.new
   end
 
   def create
+    message = "Your inquiry has been saved."
+    redir = contact_path
+
+    @inq = Inquiry.new create_params
+    @inq.status = "todo"
+    if @inq.save == false then
+      message = @inq.errors.messages
+    end
+
+    redirect_to contact_path, notice: message
   end
 
   def edit
+    @inq = Inquiry.find params[:id]
   end
 
   def update
+    message = "Inquiry has been updated."
+    redir = edit_inquiries_path
+
+    @inq = Inquiry.find params[:id]
+    if @inq.update update_params then
+      redir = inquiries_path
+    else
+      message = @inq.errors.messages
+    end
+
+    redirect_to redir, notice: message
   end
 
   def destroy
+    message = "Inquiry has been destroyed."
+    redir = edit_inquiries_path
+
+    @inq = Inquiry.find params[:id]
+    if @inq.destroy then
+      redir = inquiries_path
+    else
+      message = @inq.errors.messages
+    end
+
+    redirect_to redir, notice: message
   end
+
+  private
+  def create_params
+    params.require(:inquiry).permit(:role, :email, :body)
+  end
+
+  def update_params
+    params.require(:inquiry).permit(:role, :email, :body, :status)
+  end
+  
 end
