42c72a7a9691f893166c26d67e1b8c465d887519
Eirik Husebø <jenssen.eirik@gmail.com>
2023-03-10 19:51:47 +0100

    validate newsletter subscriber emails

--- app/models/newsletter_subscriber.rb
+++ app/models/newsletter_subscriber.rb
@@ -1,5 +1,13 @@
 class NewsletterSubscriber < ApplicationRecord
 
     validates :email, uniqueness: true
+    validate :check_email
+
+    protected
+    def check_email
+        unless email.blank?
+         validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
+        end
+    end
 
 end
