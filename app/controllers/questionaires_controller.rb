fa0525c397f688113837480316a347c0c881613b
ehu009 <jenssen.eirik@gmail.com>
2023-04-08 16:12:58 +0200

    controller method for aggregating user data

--- app/controllers/questionaires_controller.rb
+++ app/controllers/questionaires_controller.rb
@@ -7,6 +7,44 @@ class QuestionairesController < ApplicationController
     @dist_q = DistributorQuestionaire.new
   end
 
+  def questionaire_data
+    
+    p = ProducerQuestionaire.all
+    @prod_randomized = p.select(:commodities).shuffle
+    @prod_cryp_bros = p.where(:pays_crypto => true).count
+    @prod_card_bros = p.where(:pays_card => true).count
+    @prod_wire_bros = p.where(:pays_wire => true).count
+    @prod_depo_bros = p.where(:pays_deposit => true).count
+    
+    @prod_amount_sum = p.sum(:volume)
+    @prod_amount_avg = p.average(:volume)
+    pvol = p.select(:volume)
+    @prod_amount_min = pvol.min
+    @prod_amount_max = pvol.max
+
+
+    d = DistributorQuestionaire.all
+    @dist_cryp_bros = d.where(:pays_crypto => true).count
+    @dist_card_bros = d.where(:pays_card => true).count
+    @dist_wire_bros = d.where(:pays_wire => true).count
+    @dist_depo_bros = d.where(:pays_deposit => true).count
+    @dist_randomized = d.select(:other_products).shuffle
+    
+    @dist_amount_sum = d.sum(:yearly_amount)
+    @dist_amount_avg = d.average(:yearly_amount)
+    dvol = p.select(:yearly_amount)
+    @dist_amount_min = dvol.min
+    @dist_amount_max = dvol.max
+
+    @dist_age_sum = d.sum(:length)
+    @dist_age_avg = d.average(:length)
+    dage = p.select(:length)
+    @dist_age_min = dage.min
+    @dist_age_max = dage.max
+
+
+  end
+
   def fill_producer_questionaire        
     message = nil
     redir = new_producer_questionaire_path
